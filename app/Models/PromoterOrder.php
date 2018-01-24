291136b36e458feba1293dead8fa35c2d9e1344c
shenyifei <m809745357@gmail.com>
2018-01-24 22:16:38 +0800

    增加推广员可以取消身份

--- app/Models/PromoterOrder.php
+++ app/Models/PromoterOrder.php
@@ -10,7 +10,9 @@ class PromoterOrder extends Model
 
     public function department()
     {
-        return $this->belongsTo(Department::class, 'department_id');
+        return $this->belongsTo(Department::class, 'department_id')->withDefault([
+            'name' => '门诊部'
+        ]);
     }
 
     public function record()
